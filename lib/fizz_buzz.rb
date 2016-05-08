fbb5a5b4a3f22df6e03af3891c3c6b634905ce9e
MikaelFeher <mikael.feher@gmail.com>
2016-05-08 17:21:38 +0200

    Added a method and check for fizzbuzz-part

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,8 @@
 def fizz_buzz(number)
   case
+    when divisible_by_fifteen?(number) then 'fizz buzz'
     when divisible_by_three?(number) then 'fizz'
-    when divisible_by_five?(number) then 'buzz'  
+    when divisible_by_five?(number) then 'buzz'
     else number
   end
 end
@@ -12,4 +13,8 @@ end
 
 def divisible_by_five?(number)
   number % 5 == 0
+end
+
+def divisible_by_fifteen?(number)
+  number % 15 == 0
 end

42eefbf5622bb003264061ad99fa9f09a37fe1ba
MikaelFeher <mikael.feher@gmail.com>
2016-05-08 17:02:23 +0200

    Added method anf check for the buzz-part

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,10 +1,15 @@
 def fizz_buzz(number)
   case
     when divisible_by_three?(number) then 'fizz'
+    when divisible_by_five?(number) then 'buzz'  
     else number
   end
 end
 
 def divisible_by_three?(number)
   number % 3 == 0
+end
+
+def divisible_by_five?(number)
+  number % 5 == 0
 end
